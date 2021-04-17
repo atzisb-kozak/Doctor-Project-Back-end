00daede956f0ddfdbeaa94c966c74d78c9cb7ed5
atzisb-kozak <alkergoat@gmail.com>
2021-04-17 18:47:22 +0200

    [Feat](app) create login, logout and register route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -9,4 +9,19 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+	@Post('register')
+	async register() {
+		return true
+	}
+
+	@Post('login')
+	async login(@Body('username') username: string,@Body('password') password: string) {
+		return this.appService.login(username, password);
+	}
+
+	@Post('logout')
+	async logout() {
+		return true
+	}
 }
